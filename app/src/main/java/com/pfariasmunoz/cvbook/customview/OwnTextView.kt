04e26d1712e8119707e6392006310bd1154f0375
pamunoz <pfariasmunoz@gmail.com>
2018-11-12 11:44:25 -0300

    override the ondraw to be in control of the drawing process of the view

--- app/src/main/java/com/pfariasmunoz/cvbook/customview/OwnTextView.kt
+++ app/src/main/java/com/pfariasmunoz/cvbook/customview/OwnTextView.kt
@@ -1,7 +1,12 @@
 package com.pfariasmunoz.cvbook.customview
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
 import android.widget.TextView
 
-class OwnTextView(context: Context, attrs: AttributeSet) : TextView(context, attrs)
+class OwnTextView(context: Context, attrs: AttributeSet) : TextView(context, attrs) {
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+}
