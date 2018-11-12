493bfd33e8f5dc8aebe18a6ecd0785a8853946a5
pamunoz <pfariasmunoz@gmail.com>
2018-11-12 12:01:11 -0300

    Add a red background and fix display of custom view

--- app/src/main/java/com/pfariasmunoz/cvbook/customview/OwnTextView.kt
+++ app/src/main/java/com/pfariasmunoz/cvbook/customview/OwnTextView.kt
@@ -2,11 +2,23 @@ package com.pfariasmunoz.cvbook.customview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.widget.TextView
 
 class OwnTextView(context: Context, attrs: AttributeSet) : TextView(context, attrs) {
+
+    lateinit var backgroundPaint : Paint
+
+    init {
+        backgroundPaint = Paint().apply {
+            color = 0xffff0000.toInt()
+            style = Paint.Style.FILL
+        }
+    }
+
     override fun onDraw(canvas: Canvas?) {
+        canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), backgroundPaint)
         super.onDraw(canvas)
     }
 }
