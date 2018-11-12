567fabdf3bd6bc0d6e8a7c95f8f0ff350dddc6c9
pamunoz <pfariasmunoz@gmail.com>
2018-11-12 14:45:02 -0300

    Add extension method to convert pixels to dp

--- app/src/main/java/com/pfariasmunoz/cvbook/extensions/ViewExt.kt
+++ app/src/main/java/com/pfariasmunoz/cvbook/extensions/ViewExt.kt
@@ -3,3 +3,5 @@ package com.pfariasmunoz.cvbook.extensions
 import android.view.View
 
 fun View.dpToPixels(dp: Int) = (dp * resources.displayMetrics.density + 0.5).toInt()
+
+fun View.pixelsToDp(pixels: Int) = (pixels / resources.displayMetrics.density + 0.5).toInt()
