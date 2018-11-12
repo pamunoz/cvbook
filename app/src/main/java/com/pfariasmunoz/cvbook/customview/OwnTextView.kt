95a2f51825dff6653b3915d4622fa195e87392eb
pamunoz <pfariasmunoz@gmail.com>
2018-11-12 11:41:22 -0300

    Add a custom textview

---
+++ app/src/main/java/com/pfariasmunoz/cvbook/customview/OwnTextView.kt
@@ -0,0 +1,7 @@
+package com.pfariasmunoz.cvbook.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+class OwnTextView(context: Context, attrs: AttributeSet) : TextView(context, attrs)
