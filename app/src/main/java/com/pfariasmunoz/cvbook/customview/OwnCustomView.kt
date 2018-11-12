9c63af2ef9126580fcf21bda7b8e5c4bf35431c0
pamunoz <pfariasmunoz@gmail.com>
2018-11-12 16:41:10 -0300

    Set the parameterised color on the customview

--- app/src/main/java/com/pfariasmunoz/cvbook/customview/OwnCustomView.kt
+++ app/src/main/java/com/pfariasmunoz/cvbook/customview/OwnCustomView.kt
@@ -1,24 +1,41 @@
 package com.pfariasmunoz.cvbook.customview
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import com.pfariasmunoz.cvbook.R
 import com.pfariasmunoz.cvbook.extensions.TAG
 import com.pfariasmunoz.cvbook.extensions.getMeasurementSize
 
 class OwnCustomView(ctx: Context, attrs: AttributeSet) : View(ctx, attrs) {
 
-    val DEFAULT_SIZE = 1000
+    companion object {
+        private const val DEFAULT_SIZE = 1000
+
+        private const val DEFAULT_FILL_COLOR = Color.RED
+    }
+
+
 
     private val backgroundPaint : Paint = Paint()
+    var fillColor: Int = DEFAULT_FILL_COLOR
 
     init {
         backgroundPaint.apply {
             color = 0xffff0000.toInt()
             style = Paint.Style.FILL
+        }
+        // get the parameterised color
+        val ta: TypedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.OwnCustomView, 0, 0)
+        try {
+            fillColor = ta.getColor(R.styleable.OwnCustomView_fillColor, DEFAULT_FILL_COLOR)
+        } finally {
+            ta.recycle()
         }
     }
 
